f6f843a653a24a0f3b702daaf186433a16dfca7f
vamshibonagiri <106440029+vamshibonagiri@users.noreply.github.com>
2024-06-19 21:51:01 +0530

    Update Kth_Smallest_Element.cpp

--- Heaps/Kth_Smallest_Element.cpp
+++ Heaps/Kth_Smallest_Element.cpp
@@ -1,3 +1,31 @@
+// You are given an integer K and an array of distinct integers of size N. Find the Kth smallest element from the array.
+// Note: Try solving using MaxHeap or Quick Select, don't solve using sorting.
+
+// Input Format
+
+// First line of input contains T - number of test cases. For each test case, the first line contains N - the size of the array. The second line contains N integers - the elements of the array.
+
+// Constraints
+
+// 1 <= T <= 100
+// 1 <= K <= N <= 105
+// 0 <= A[i] <= 105
+
+// Output Format
+
+// For each test case, print the Kth smallest element from the array, separated by newline.
+
+// Sample Input 0
+
+// 2
+// 5 3
+// 1 5 13 2 8
+// 6 2
+// 10 1 20 8 31 12
+// Sample Output 0
+
+// 5
+// 8
 #include <bits/stdc++.h>
 using namespace std;
 
